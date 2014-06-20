5b43e6891f3101f47aa838cdc41dfa91caed4d3e
Scott Swanson <sms2fx@captechventures.com>
2014-06-20 14:30:07 -0400

    google api

--- client/angular/controller/controllers.js
+++ client/angular/controller/controllers.js
@@ -1,7 +1,8 @@
 app.controller('scraperController', function($scope, Restangular){
 
-		Restangular.all('/localhost:8081/scrape').getList().then(function(bill) {
-			$scope.message = bill;
+		Restangular.all('/localhost:8081/scrape').getList().then(function(result) {
+			$scope.message = result;
+			console.log(result);
 		});
 
 })
