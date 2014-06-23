db98a46aaff34e26d1e7ded290779e4b8a8c9915
Daniel Golman <dgolman@vt.edu>
2014-06-23 16:21:20 -0400

    first commit

--- client/angular/controller/controllers.js
+++ client/angular/controller/controllers.js
@@ -1,40 +1,53 @@
-app.controller('scraperController', function($scope, Restangular, $filter){
+app.factory('geocode', function($q, Restangular, $timeout) {
+	
+	return function(restaurants, from) {
+
+		var promises = restaurants.map(function(restaurant){
+			
+			
+
+			var address = restaurant.contact.split('\u2022')[0];
+			
+			Restangular.oneUrl('maps', 'https://maps.googleapis.com/maps/api/geocode/json?address='+address+',+Richmond,+VA').get().then(function(result){
+				var deferred = $q.defer();
+				var to  = new google.maps.LatLng(result.results[0].geometry.location.lat, result.results[0].geometry.location.lng);
+				var dist = google.maps.geometry.spherical.computeDistanceBetween(from, to) * 0.000621371;
+				restaurant.distance = dist;
+				deferred.resolve(restaurant);
+				return deferred.promise;
+			});
+			
+		});
+
+		return $q.all(promises);
+	}
+});
+
+app.controller('scraperController', function($scope, Restangular, $filter, $q, geocode){
 	function success(position) { 
 		//console.log(position);
 			// stackoverflow: distance between 2 geo locations
 			var from = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
-			Restangular.all('/localhost:8081/scrape').getList().then(function(result) {
-				var restaurants = result;
-				var ret = [];
-				var log = [];
-				angular.forEach(restaurants, function(restaurant, key){
-					
-					setTimeout(function() {
-						var address = restaurant.contact.split('\u2022')[0];
-						Restangular.oneUrl('maps', 'https://maps.googleapis.com/maps/api/geocode/json?address='+address+',+Richmond,+VA').get().then(function(result){
-								var to   = new google.maps.LatLng(result.results[0].geometry.location.lat, result.results[0].geometry.location.lng);
-								var dist = google.maps.geometry.spherical.computeDistanceBetween(from, to) * 0.000621371;
-										console.log(dist);
-								restaurant.distance = dist;
-						});
-					}, 100);
+			Restangular.all('/localhost:8081/scrape').getList().then(function(restaurants) {
 
+				geocode(restaurants, from).then(function(restaurants) {
+					$scope.restaurants = restaurants;
+					console.log(restaurants);
 				});
-				
-				$scope.restaurants = restaurants;
+
 			});
 		}
+
 		function error(msg) {
 			console.log(msg);
 		}
+
 		if (navigator.geolocation) {
 			navigator.geolocation.getCurrentPosition(success, error);
 		} else {
 			error('not supported');
 		}
 
-
-	})
-
+	});
 
